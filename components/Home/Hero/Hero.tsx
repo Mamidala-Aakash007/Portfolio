d46ca4304ea3703dbdc4ddefe5e326e206451b6c
Mamidala-Aakash007 <luckyaakashavi1@gmail.com>
2025-09-21 20:17:11 +0530

    apostrphy fix

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -1,15 +1,14 @@
-"use client"
+"use client";
 
 import React from "react";
 import Image from "next/image";
-import { Typewriter } from 'react-simple-typewriter'
+import { Typewriter } from "react-simple-typewriter";
 import { BsArrowRight } from "react-icons/bs";
 import ParticlesHero from "./ParticleBackground";
 
 const Hero = () => {
   return (
     <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
-
       <ParticlesHero />
       <div className="relative z-10 flex flex-col items-center">
         <Image
@@ -23,20 +22,22 @@ const Hero = () => {
           Creating web products, <br />
           <span className="text-cyan-200"> brands, and experience.</span>{" "}
         </h1>
-        <h2 className="mt-5 text-sm px-2 text-center sm:text-2xl font-medium flex items-center">Hi!, I'm Aakash - A Passionate
-            <span className="text-cyan-200 font-bold pl-2">
-                <Typewriter words={[
-                        'Frontend Developer',
-                        'Backend Developer',
-                        'Web Developer',
-                    ]}
-                    loop={true}
-                    delaySpeed={75}
-                    deleteSpeed={50}
-                    cursor= {true}
-                    cursorStyle='|'
-                />
-            </span>
+        <h2 className="mt-5 text-sm px-2 text-center sm:text-2xl font-medium flex items-center">
+          Hi!, I&apos;m Aakash - A Passionate
+          <span className="text-cyan-200 font-bold pl-2">
+            <Typewriter
+              words={[
+                "Frontend Developer",
+                "Backend Developer",
+                "Web Developer",
+              ]}
+              loop={true}
+              delaySpeed={75}
+              deleteSpeed={50}
+              cursor={true}
+              cursorStyle="|"
+            />
+          </span>
         </h2>
         <button className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor pointer rounded-full text-lg text-medium">
           <span>See my work</span>
