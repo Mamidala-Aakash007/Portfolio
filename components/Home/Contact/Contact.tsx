91a9bf3f29a2beefca9be5d12ef38d4c33e342b0
Mamidala-Aakash007 <luckyaakashavi1@gmail.com>
2025-09-27 18:15:42 +0530

    fix contact form

--- components/Home/Contact/Contact.tsx
+++ components/Home/Contact/Contact.tsx
@@ -58,34 +58,41 @@ const Contact = () => {
           </div>
         </div>
         {/* form */}
+        <form action="">
         <div
           data-aos="zoom-in"
-          data-aos-anchor-placement="top-center"
+          
           className="md:p-10 p-5 bg-[#131332] rounded-lg"
         >
+         
           <input
             type="text"
+            name="name"
             placeholder="Name"
             className="px-4 py-3.5 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70"
           />
           <input
             type="email"
+            name="email"
             placeholder="Email Address"
             className="px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70"
           />
           <input
             type="text"
+            name="phone"
             placeholder="Mobile Number"
             className="px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70"
           />
           <textarea
             placeholder="Your Message"
+            name="message"
             className="px-4 py-3.5 mt-6 bg-[#363659] text-white outline-none rounded-md w-full placeholder:text-white/70 h-[10rem]"
           ></textarea>
           <button className="mt-8 px-12 py-4 bg-blue-950 hover:bg-blue-900 transition-all duration-300 cursor-pointer text-white rounded-full">
             Send Message
           </button>
         </div>
+        </form>
       </div>
     </div>
   );
--- components/Home/Projects/Projects.tsx
+++ components/Home/Projects/Projects.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 export const Projects = () => {
   return (
@@ -11,13 +12,14 @@ export const Projects = () => {
       <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
         {/* first project */}
         <div data-aos="fade-up" data-aos-anchor-placement="top-center">
+          <Link href="https://social-media-dashboard-dark-light-t.vercel.app/" target="_blank" rel="noopener noreferrer">
           <Image
             src="/images/Social_media_dashboard.png"
             alt="img"
             width={800}
             height={650}
             className="rounded-lg cursor-pointer"
-          />
+          /></Link>
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             Modern Social DashBoard
           </h1>
@@ -46,14 +48,14 @@ export const Projects = () => {
           data-aos="fade-up"
           data-aos-anchor-placement="top-center"
           data-aos-delay="200"
-        >
+        ><Link href="https://movies-search-engine-tau.vercel.app/" target="_blank" rel="noopener noreferrer">
           <Image
             src="/images/MovieFinder.png"
             alt="img"
             width={800}
             height={650}
             className="rounded-lg cursor-pointer"
-          />
+          /></Link>
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             Movie Finder
           </h1>
@@ -64,14 +66,14 @@ export const Projects = () => {
           data-aos="fade-up"
           data-aos-anchor-placement="top-center"
           data-aos-delay="200"
-        >
+        ><Link href="https://book-finder-gray-eta.vercel.app/" target="_blank" rel="noopener noreferrer">
           <Image
             src="/images/bookfinder.png"
             alt="img"
             width={800}
             height={650}
             className="rounded-lg cursor-pointer"
-          />
+          /></Link>
           <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">
             Library
           </h1>
