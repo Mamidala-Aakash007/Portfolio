e8d6272d3c9a9bd3b0194b82fbd5031e1142d455
Mamidala-Aakash007 <luckyaakashavi1@gmail.com>
2025-09-28 14:12:09 +0530

    add profile pic

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -6,10 +6,8 @@ import { Typewriter } from "react-simple-typewriter";
 import { BsArrowRight } from "react-icons/bs";
 import ParticlesHero from "./ParticleBackground";
 
-
-export default function Her () {
-
-    const handleNavClick = (id: string) => {
+export default function Her() {
+  const handleNavClick = (id: string) => {
     const el = document.getElementById(id);
     if (el) el.scrollIntoView({ behavior: "smooth" });
   };
@@ -18,19 +16,29 @@ export default function Her () {
     <div className="relative h-screen flex items-center justify-center text-white overflow-hidden flex-col">
       <ParticlesHero />
       <div className="relative z-10 flex flex-col items-center">
-        <Image
-          src="/images/s1.jpg"
-          alt="heroimage"
-          width={150}
-          height={150}
-          className="rounded-full border-8 border-[#0c0c48aa]"
+        <div className="w-[150px] h-[150px] rounded-full overflow-hidden border-8 border-[#0c0c48aa]">
+          <Image
+            src="/images/Ai.png"
+            alt="heroimage"
+            width={150}
+            height={150}
+            className="w-full h-full object-cover"
+            data-aos="fade-up"
+          />
+        </div>
+        <h1
           data-aos="fade-up"
-        />
-        <h1 data-aos="fade-up" data-aos-delay="200" className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide">
+          data-aos-delay="200"
+          className="text-2xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 text-center font-bold tracking-wide"
+        >
           Creating web products, <br />
           <span className="text-cyan-200"> brands, and experience.</span>{" "}
         </h1>
-        <h2 data-aos="fade-up" data-aos-delay="400" className="mt-5 text-sm px-2 text-center sm:text-2xl font-medium flex items-center">
+        <h2
+          data-aos="fade-up"
+          data-aos-delay="400"
+          className="mt-5 text-sm px-2 text-center sm:text-2xl font-medium flex items-center"
+        >
           Hi!, I&apos;m Aakash - A Passionate
           <span className="text-cyan-200 font-bold pl-2">
             <Typewriter
@@ -47,13 +55,16 @@ export default function Her () {
             />
           </span>
         </h2>
-        <button onClick={()=> handleNavClick("works")}data-aos="fade-up" data-aos-delay="600" className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor pointer rounded-full text-lg text-medium">
+        <button
+          onClick={() => handleNavClick("works")}
+          data-aos="fade-up"
+          data-aos-delay="600"
+          className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor pointer rounded-full text-lg text-medium"
+        >
           <span>See my work</span>
           <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
         </button>
       </div>
     </div>
   );
-};
-
-
+}
